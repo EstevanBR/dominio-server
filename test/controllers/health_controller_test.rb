fe1fa53e34393cf6fad1a3ab5fd1843d0af223e9
EstevanBR <estevan@bluerocket.us>
2019-09-09 19:51:26 -0700

    check health response body

--- test/controllers/health_controller_test.rb
+++ test/controllers/health_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class HealthControllerTest < ActionDispatch::IntegrationTest
   test "health" do
     get health_url, as: :json
-    byebug
+    assert parsed_response == JSON.parse({STATUS: "UP"}.to_json)
     assert_response :ok
   end
 end
